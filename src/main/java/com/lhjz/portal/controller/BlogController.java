193aa79e98e0fcb74ff7bec69df547131957e264
weicheng xi <weicheng.xi@newtouch.cn>
2017-04-27 19:11:57 +0800

    博文评论分享api实现

--- src/main/java/com/lhjz/portal/controller/BlogController.java
+++ src/main/java/com/lhjz/portal/controller/BlogController.java
@@ -718,6 +718,88 @@ public class BlogController extends BaseController {
 		});
 		return RespBody.succeed();
 	}
+	
+	@RequestMapping(value = "comment/share", method = RequestMethod.POST)
+	@ResponseBody
+	public RespBody shareComment(@RequestParam("basePath") String basePath, @RequestParam("id") Long id,
+			@RequestParam("href") final String href, @RequestParam("html") String html,
+			@RequestParam(value = "desc", required = false) String desc,
+			@RequestParam(value = "users", required = false) String users,
+			@RequestParam(value = "channels", required = false) String channels,
+			@RequestParam(value = "mails", required = false) String mails) {
+
+		Comment comment = commentRepository.findOne(id);
+
+		final User loginUser = getLoginUser();
+
+		final String html2 = StringUtil.replace(
+				"<h1 style=\"color: blue;\">分享博文评论: <a target=\"_blank\" href=\"{?1}\">{?2}</a></h1><hr/>{?3}", href,
+				"博文评论链接", html);
+
+		final String title = StringUtil.isNotEmpty(desc) ? desc : "下面的博文评论有分享到你";
+
+		Mail mail = Mail.instance();
+		if (StringUtil.isNotEmpty(users)) {
+			Stream.of(users.split(",")).forEach(username -> {
+				User user = getUser(username);
+				if (user != null) {
+					mail.addUsers(user);
+
+					ChatDirect chatDirect = new ChatDirect();
+					chatDirect.setChatTo(user);
+					chatDirect.setContent(
+							StringUtil.replace("## ~私聊消息播报~\n> 来自 {~{?1}} 的博文评论分享:  [{?2}]({?3})\n\n---\n\n{?4}",
+									loginUser.getUsername(), "博文评论链接", href, comment.getContent()));
+
+					chatDirectRepository.saveAndFlush(chatDirect);
+				}
+			});
+		}
+		if (StringUtil.isNotEmpty(channels)) {
+			Stream.of(channels.split(",")).forEach(name -> {
+				Channel channel = channelRepository.findOneByName(name);
+				if (channel != null) {
+					channel.getMembers().forEach(user -> {
+						mail.addUsers(user);
+					});
+
+					ChatChannel chatChannel = new ChatChannel();
+					chatChannel.setChannel(channel);
+					chatChannel.setContent(
+							StringUtil.replace("## ~频道消息播报~\n> 来自 {~{?1}} 的博文评论分享:  [{?2}]({?3})\n\n---\n\n{?4}",
+									loginUser.getUsername(), "博文评论链接", href, comment.getContent()));
+
+					chatChannelRepository.saveAndFlush(chatChannel);
+				}
+			});
+		}
+
+		if (StringUtil.isNotEmpty(mails)) {
+			Stream.of(mails.split(",")).forEach(m -> {
+				if (ValidateUtil.isEmail(m)) {
+					mail.add(m);
+				}
+			});
+		}
+
+		ThreadUtil.exec(() -> {
+
+			try {
+				Thread.sleep(3000);
+				mailSender
+						.sendHtml(String.format("TMS-博文评论分享_%s", DateUtil.format(new Date(), DateUtil.FORMAT7)),
+								TemplateUtil.process("templates/mail/mail-dynamic", MapUtil.objArr2Map("user",
+										loginUser, "date", new Date(), "href", href, "title", title, "content", html2)),
+								mail.get());
+				logger.info("博文评论分享邮件发送成功！");
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("博文评论分享邮件发送失败！");
+			}
+
+		});
+		return RespBody.succeed();
+	}
 
 	@RequestMapping(value = "comment/create", method = RequestMethod.POST)
 	@ResponseBody
