a0d04c8c4278525942f3029e69e57a7ed8ba9230
weicheng <weicheng.xi@newtouch.cn>
2017-05-12 11:27:31 +0800

    ...

--- src/main/java/com/lhjz/portal/config/OAuth2ClientConfig.java
+++ src/main/java/com/lhjz/portal/config/OAuth2ClientConfig.java
@@ -33,7 +33,7 @@ public class OAuth2ClientConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+		http.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 	}
 
 	@Bean
