f5926940835e6cbaf08ec3ea4e940af225eeaaec
xiweicheng <xiweicheng@yeah.net>
2017-08-09 15:36:35 +0800

    邮件队列使用非阻塞队列

--- src/main/java/com/lhjz/portal/component/core/MailQueue.java
+++ src/main/java/com/lhjz/portal/component/core/MailQueue.java
@@ -4,11 +4,7 @@ import com.lhjz.portal.component.core.model.MailItem;
 
 public interface MailQueue {
 
-	void put(MailItem mailItem) throws InterruptedException;
-	
 	boolean offer(MailItem mailItem);
-	
-	MailItem take() throws InterruptedException;
 	
 	MailItem poll();
 	
