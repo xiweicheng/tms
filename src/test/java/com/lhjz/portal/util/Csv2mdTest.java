d0a8fa037bbfa8bbecd7f2f4930f8500305b3872
xiweicheng <xiweicheng@yeah.net>
2018-05-26 21:38:16 +0800

    opencsv

--- src/test/java/com/lhjz/portal/util/Csv2mdTest.java
+++ src/test/java/com/lhjz/portal/util/Csv2mdTest.java
@@ -1,9 +1,16 @@
 package com.lhjz.portal.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+
+import com.opencsv.CSVReader;
 
 public class Csv2mdTest {
 
@@ -32,6 +39,28 @@ public class Csv2mdTest {
 		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void test() throws IOException {
+		File file = new File("/Users/xiweicheng/temp/test.csv");
+		//		CSVReader csvReader = new CSVReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+		CSVReader csvReader = new CSVReader(new InputStreamReader(new FileInputStream(file), Charset.forName("GBK")));
+		//		String[] strs = csvReader.readNext();
+		//		if (strs != null && strs.length > 0) {
+		//			for (String str : strs)
+		//				if (null != str && !str.equals(""))
+		//					System.out.print(str + " , ");
+		//			System.out.println("\n---------------");
+		//		}
+		List<String[]> list = csvReader.readAll();
+		for (String[] ss : list) {
+			for (String s : ss)
+				if (null != s && !s.equals(""))
+					System.out.print(s + " , ");
+			System.out.println();
+		}
+		csvReader.close();
 	}
 
 }
--- src/test/java/com/lhjz/portal/util/ExcelReadTest.java
+++ src/test/java/com/lhjz/portal/util/ExcelReadTest.java
@@ -70,46 +70,49 @@ public class ExcelReadTest {
 	// 读取，指定sheet表及数据
 	//	@Test
 	public void showExcel2() throws Exception {
-		HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(new File("/Users/xiweicheng/temp/test.xls")));
-		HSSFSheet sheet = null;
-		int i = workbook.getSheetIndex("xt"); // sheet表名
-		sheet = workbook.getSheetAt(i);
-		for (int j = 0; j < sheet.getLastRowNum() + 1; j++) {// getLastRowNum
-																// 获取最后一行的行标
-			HSSFRow row = sheet.getRow(j);
-			if (row != null) {
-				for (int k = 0; k < row.getLastCellNum(); k++) {// getLastCellNum
-																// 是获取最后一个不为空的列是第几个
-					if (row.getCell(k) != null) { // getCell 获取单元格数据
-						System.out.print(row.getCell(k) + "\t");
-					} else {
-						System.out.print("\t");
+		try (HSSFWorkbook workbook = new HSSFWorkbook(
+				new FileInputStream(new File("/Users/xiweicheng/temp/test.xls")))) {
+			HSSFSheet sheet = null;
+			int i = workbook.getSheetIndex("xt"); // sheet表名
+			sheet = workbook.getSheetAt(i);
+			for (int j = 0; j < sheet.getLastRowNum() + 1; j++) {// getLastRowNum
+																	// 获取最后一行的行标
+				HSSFRow row = sheet.getRow(j);
+				if (row != null) {
+					for (int k = 0; k < row.getLastCellNum(); k++) {// getLastCellNum
+																	// 是获取最后一个不为空的列是第几个
+						if (row.getCell(k) != null) { // getCell 获取单元格数据
+							System.out.print(row.getCell(k) + "\t");
+						} else {
+							System.out.print("\t");
+						}
 					}
 				}
+				System.out.println("");
 			}
-			System.out.println("");
 		}
 	}
 
 	// 写入，往指定sheet表的单元格
 	//	@Test
 	public void insertExcel3() throws Exception {
-		HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(new File("/Users/xiweicheng/temp/test.xls"))); // 读取的文件
-		HSSFSheet sheet = null;
-		int i = workbook.getSheetIndex("xt"); // sheet表名
-		sheet = workbook.getSheetAt(i);
+		try (HSSFWorkbook workbook = new HSSFWorkbook(
+				new FileInputStream(new File("/Users/xiweicheng/temp/test.xls")))) { // 读取的文件
+			HSSFSheet sheet = null;
+			int i = workbook.getSheetIndex("xt"); // sheet表名
+			sheet = workbook.getSheetAt(i);
 
-		HSSFRow row = sheet.getRow(0); // 获取指定的行对象，无数据则为空，需要创建
-		if (row == null) {
-			row = sheet.createRow(0); // 该行无数据，创建行对象
+			HSSFRow row = sheet.getRow(0); // 获取指定的行对象，无数据则为空，需要创建
+			if (row == null) {
+				row = sheet.createRow(0); // 该行无数据，创建行对象
+			}
+
+			Cell cell = row.createCell(1); // 创建指定单元格对象。如本身有数据会替换掉
+			cell.setCellValue("tt"); // 设置内容
+
+			FileOutputStream fo = new FileOutputStream("E:/temp/t1.xls"); // 输出到文件
+			workbook.write(fo);
 		}
-
-		Cell cell = row.createCell(1); // 创建指定单元格对象。如本身有数据会替换掉
-		cell.setCellValue("tt"); // 设置内容
-
-		FileOutputStream fo = new FileOutputStream("E:/temp/t1.xls"); // 输出到文件
-		workbook.write(fo);
-
 	}
 
 }
