61427b6dff739cc794ecb80e2c6f843f491de5d6
xiweicheng <xiweicheng@yeah.net>
2018-07-25 21:48:05 +0800

    ...

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+sed -i '' 's/=dev/=prod/' src/main/resources/application.properties
+
 mvn clean package -Dmaven.test.skip=true
+
+sed -i '' 's/=prod/=dev/' src/main/resources/application.properties
 
 docker build -t xiwc/tms .
 
