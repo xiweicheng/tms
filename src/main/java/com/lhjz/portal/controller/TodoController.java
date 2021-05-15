34c51b5f38bfecdc17e7351cca9169aa30efcd79
xiweicheng <xiweicheng@yeah.net>
2021-05-15 12:36:14 +0800

    合并前端静态打包资源

--- src/main/java/com/lhjz/portal/controller/TodoController.java
+++ src/main/java/com/lhjz/portal/controller/TodoController.java
@@ -3,24 +3,6 @@
  */
 package com.lhjz.portal.controller;
 
-import java.util.List;
-import java.util.stream.Stream;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.google.common.collect.Lists;
 import com.lhjz.portal.base.BaseController;
 import com.lhjz.portal.component.MailSender;
@@ -35,175 +17,193 @@ import com.lhjz.portal.repository.TodoRepository;
 import com.lhjz.portal.repository.UserRepository;
 import com.lhjz.portal.util.JsonUtil;
 import com.lhjz.portal.util.StringUtil;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
- * 
  * @author xi
- * 
  * @date 2015年3月28日 下午1:19:05
- * 
  */
 @Controller
 @RequestMapping("admin/todo")
 public class TodoController extends BaseController {
 
-	@Autowired
-	TodoRepository todoRepository;
+    @Autowired
+    TodoRepository todoRepository;
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-	@Autowired
-	LogRepository logRepository;
+    @Autowired
+    LogRepository logRepository;
 
-	@Autowired
-	MailSender mailSender;
+    @Autowired
+    MailSender mailSender;
 
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	@ResponseBody
-	public RespBody create(@RequestParam("title") String title,
-			@RequestParam(value = "sortIndex", required = false) Long sortIndex,
-			@RequestParam(value = "content", required = false) String content) {
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBody create(@RequestParam("title") String title,
+                           @RequestParam(value = "sortIndex", required = false) Long sortIndex,
+                           @RequestParam(value = "content", required = false) String content) {
 
-		if (StringUtil.isEmpty(title)) {
-			return RespBody.failed("标题不能为空!");
-		}
+        if (StringUtil.isEmpty(title)) {
+            return RespBody.failed("标题不能为空!");
+        }
 
-		Todo todo = Todo.builder().title(title).content(content).sortIndex(sortIndex).build();
+        Todo todo = Todo.builder().title(title).content(content).sortIndex(sortIndex).build();
 
-		Todo todo2 = todoRepository.saveAndFlush(todo);
+        Todo todo2 = todoRepository.saveAndFlush(todo);
 
-		return RespBody.succeed(todo2);
-	}
+        return RespBody.succeed(todo2);
+    }
 
-	@RequestMapping(value = "listMy", method = RequestMethod.GET)
-	@ResponseBody
-	public RespBody listMy(@SortDefault(value = "id", direction = Direction.DESC) Sort sort) {
+    @RequestMapping(value = "listMy", method = RequestMethod.GET)
+    @ResponseBody
+    public RespBody listMy(@SortDefault(value = "id", direction = Direction.DESC) Sort sort) {
 
-		List<Todo> todos = todoRepository.findByStatusNotAndCreator(Status.Deleted, getLoginUser(), sort);
+        List<Todo> todos = todoRepository.findByStatusNotAndCreator(Status.Deleted, getLoginUser(), sort);
 
-		return RespBody.succeed(todos);
-	}
+        return RespBody.succeed(todos);
+    }
 
-	@RequestMapping(value = "listMy/done", method = RequestMethod.GET)
-	@ResponseBody
-	public RespBody listMyDone(@PageableDefault(sort = { "id" }, direction = Direction.DESC) Pageable pageable) {
+    @RequestMapping(value = "listMy/done", method = RequestMethod.GET)
+    @ResponseBody
+    public RespBody listMyDone(@PageableDefault(sort = {"id"}, direction = Direction.DESC) Pageable pageable,
+                               @RequestParam(value = "search", required = false) String search) {
 
-		Page<Todo> page = todoRepository.findByStatusAndCreator(Status.Done, getLoginUser(), pageable);
+        Page<Todo> page;
 
-		return RespBody.succeed(page);
-	}
+        if (StringUtils.isBlank(search)) {
+            page = todoRepository.findByStatusAndCreator(Status.Done, getLoginUser(), pageable);
+        } else {
+            page = todoRepository.findByStatusAndCreatorAndTitleContainingIgnoreCaseOrStatusAndCreatorAndContentContainingIgnoreCase(
+                    Status.Done, getLoginUser(), search, Status.Done, getLoginUser(), search, pageable);
+        }
 
-	@RequestMapping(value = "listMy/undone", method = RequestMethod.GET)
-	@ResponseBody
-	public RespBody listMyUnDone(@SortDefault(value = "id", direction = Direction.DESC) Sort sort) {
+        return RespBody.succeed(page);
+    }
 
-		List<Todo> todos = todoRepository.findByStatusInAndCreator(Lists.newArrayList(Status.New, Status.Doing),
-				getLoginUser(), sort);
+    @RequestMapping(value = "listMy/undone", method = RequestMethod.GET)
+    @ResponseBody
+    public RespBody listMyUnDone(@SortDefault(value = "id", direction = Direction.DESC) Sort sort) {
 
-		return RespBody.succeed(todos);
-	}
+        List<Todo> todos = todoRepository.findByStatusInAndCreator(Lists.newArrayList(Status.New, Status.Doing),
+                getLoginUser(), sort);
 
-	@RequestMapping(value = "update", method = RequestMethod.POST)
-	@ResponseBody
-	public RespBody update(@RequestParam("id") Long id, @RequestParam(value = "title", required = false) String title,
-			@RequestParam(value = "sortIndex", required = false) Long sortIndex,
-			@RequestParam(value = "priority", required = false) TodoPriority priority,
-			@RequestParam(value = "status", required = false) Status status,
-			@RequestParam(value = "content", required = false) String content) {
+        return RespBody.succeed(todos);
+    }
 
-		Todo todo = todoRepository.findOne(id);
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBody update(@RequestParam("id") Long id, @RequestParam(value = "title", required = false) String title,
+                           @RequestParam(value = "sortIndex", required = false) Long sortIndex,
+                           @RequestParam(value = "priority", required = false) TodoPriority priority,
+                           @RequestParam(value = "status", required = false) Status status,
+                           @RequestParam(value = "content", required = false) String content) {
 
-		if (!isSuperOrCreator(todo.getCreator())) {
-			return RespBody.failed("权限不足！");
-		}
+        Todo todo = todoRepository.findOne(id);
 
-		boolean needUpdate = false;
+        if (!isSuperOrCreator(todo.getCreator())) {
+            return RespBody.failed("权限不足！");
+        }
 
-		if (StringUtil.isNotEmpty(title)) {
-			todo.setTitle(title);
-			needUpdate = true;
-		}
+        boolean needUpdate = false;
 
-		if (StringUtil.isNotEmpty(content)) {
-			todo.setContent(content);
-			needUpdate = true;
-		}
+        if (StringUtil.isNotEmpty(title)) {
+            todo.setTitle(title);
+            needUpdate = true;
+        }
 
-		if (sortIndex != null) {
-			todo.setSortIndex(sortIndex);
-			needUpdate = true;
-		}
+        if (StringUtil.isNotEmpty(content)) {
+            todo.setContent(content);
+            needUpdate = true;
+        }
 
-		if (priority != null) {
-			todo.setPriority(priority);
-			needUpdate = true;
-		}
+        if (sortIndex != null) {
+            todo.setSortIndex(sortIndex);
+            needUpdate = true;
+        }
 
-		if (status != null) {
-			todo.setStatus(status);
-			needUpdate = true;
-		}
+        if (priority != null) {
+            todo.setPriority(priority);
+            needUpdate = true;
+        }
 
-		if (!needUpdate) {
-			return RespBody.failed("没有更新内容！");
-		}
+        if (status != null) {
+            todo.setStatus(status);
+            needUpdate = true;
+        }
 
-		Todo todo2 = todoRepository.saveAndFlush(todo);
+        if (!needUpdate) {
+            return RespBody.failed("没有更新内容！");
+        }
 
-		return RespBody.succeed(todo2);
+        Todo todo2 = todoRepository.saveAndFlush(todo);
 
-	}
+        return RespBody.succeed(todo2);
 
-	@RequestMapping(value = "delete/{id}", method = RequestMethod.POST)
-	@ResponseBody
-	public RespBody delete(@PathVariable("id") Long id) {
+    }
 
-		Todo todo = todoRepository.findOne(id);
+    @RequestMapping(value = "delete/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public RespBody delete(@PathVariable("id") Long id) {
 
-		if (!isSuperOrCreator(todo.getCreator())) {
-			return RespBody.failed("权限不足！");
-		}
+        Todo todo = todoRepository.findOne(id);
 
-		todo.setStatus(Status.Deleted);
+        if (!isSuperOrCreator(todo.getCreator())) {
+            return RespBody.failed("权限不足！");
+        }
 
-		todoRepository.saveAndFlush(todo);
+        todo.setStatus(Status.Deleted);
 
-		return RespBody.succeed(id);
-	}
+        todoRepository.saveAndFlush(todo);
 
-	@RequestMapping(value = "get/{id}", method = RequestMethod.GET)
-	@ResponseBody
-	public RespBody get(@PathVariable("id") Long id) {
+        return RespBody.succeed(id);
+    }
 
-		Todo todo = todoRepository.findOne(id);
+    @RequestMapping(value = "get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public RespBody get(@PathVariable("id") Long id) {
 
-		if (!isSuperOrCreator(todo.getCreator())) {
-			return RespBody.failed("权限不足！");
-		}
+        Todo todo = todoRepository.findOne(id);
 
-		return RespBody.succeed(todo);
-	}
-	
-	@PostMapping("sort")
-	@ResponseBody
-	public RespBody sort(TodoSortForm todoSortForm) {
+        if (!isSuperOrCreator(todo.getCreator())) {
+            return RespBody.failed("权限不足！");
+        }
 
-		if (todoSortForm == null || StringUtil.isEmpty(todoSortForm.getItems())) {
-			return RespBody.failed("参数错误！");
-		}
+        return RespBody.succeed(todo);
+    }
 
-		TodoSortItem[] sortItems = JsonUtil.json2Object(todoSortForm.getItems(), TodoSortItem[].class);
+    @PostMapping("sort")
+    @ResponseBody
+    public RespBody sort(TodoSortForm todoSortForm) {
 
-		Stream.of(sortItems).forEach(item -> {
-			Todo todo = todoRepository.findOne(item.getId());
-			if (item.getSort() != null & !item.getSort().equals(todo.getSortIndex())) {
-				todo.setSortIndex(item.getSort());
-				todoRepository.saveAndFlush(todo);
-			}
-		});
+        if (todoSortForm == null || StringUtil.isEmpty(todoSortForm.getItems())) {
+            return RespBody.failed("参数错误！");
+        }
 
-		return RespBody.succeed();
-	}
+        TodoSortItem[] sortItems = JsonUtil.json2Object(todoSortForm.getItems(), TodoSortItem[].class);
+
+        Stream.of(sortItems).forEach(item -> {
+            Todo todo = todoRepository.findOne(item.getId());
+            if (item.getSort() != null & !item.getSort().equals(todo.getSortIndex())) {
+                todo.setSortIndex(item.getSort());
+                todoRepository.saveAndFlush(todo);
+            }
+        });
+
+        return RespBody.succeed();
+    }
 
 }
--- src/main/java/com/lhjz/portal/repository/TodoRepository.java
+++ src/main/java/com/lhjz/portal/repository/TodoRepository.java
@@ -16,18 +16,18 @@ import com.lhjz.portal.entity.security.User;
 import com.lhjz.portal.pojo.Enum.Status;
 
 /**
- * 
  * @author xi
- * 
  * @date 2015年3月28日 下午2:09:06
- * 
  */
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-	List<Todo> findByStatusNotAndCreator(Status status, User creator, Sort sort);
-	
-	List<Todo> findByStatusInAndCreator(Collection<Status> status, User creator, Sort sort);
+    List<Todo> findByStatusNotAndCreator(Status status, User creator, Sort sort);
 
-	Page<Todo> findByStatusAndCreator(Status status, User creator, Pageable pageable);
+    List<Todo> findByStatusInAndCreator(Collection<Status> status, User creator, Sort sort);
+
+    Page<Todo> findByStatusAndCreator(Status status, User creator, Pageable pageable);
+
+    Page<Todo> findByStatusAndCreatorAndTitleContainingIgnoreCaseOrStatusAndCreatorAndContentContainingIgnoreCase(
+            Status status, User creator, String searchT, Status status2, User creator2, String searchC, Pageable pageable);
 
 }
